af1ac2e37ad25d0d5b1369ced9d3572dba4ec8bd
Android-罗志敏 <54543534@domain.com>
2017-04-13 13:51:45 +0800

    Initial commit

---
+++ app/src/main/java/com/kxt/goldtransaction/MainActivity.java
@@ -0,0 +1,53 @@
+package com.kxt.goldtransaction;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.vilyever.socketclient.SocketClient;
+import com.vilyever.socketclient.SocketResponsePacket;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultText;
+    private SocketClient socketClient;
+    private String url="117.141.138.101";
+    private  int prot=41901;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultText= (TextView) findViewById(R.id.resultText);
+        initSocket();
+    }
+
+    private void initSocket() {
+        socketClient = new SocketClient(url, prot);
+        socketClient.registerSocketDelegate(new SocketClient.SocketDelegate(){
+            @Override
+            public void onConnected(SocketClient client) {
+                Log.d("socketTO","onConnected.......");
+                socketClient.send("hello, server !--------------------------->Android");
+//                socketClient.setHeartBeatMessage("hello, server !--------------------------->Android");
+            }
+
+            @Override
+            public void onDisconnected(SocketClient client) {
+                Log.d("socketTO","onDisconnected.......");
+
+            }
+
+            @Override
+            public void onResponse(SocketClient client, @NonNull SocketResponsePacket responsePacket) {
+                Log.d("socketTO","onResponse.......");
+            }
+        });
+        socketClient.setConnectionTimeout(1000 * 15);
+        socketClient.setHeartBeatInterval(1000);
+        socketClient.setRemoteNoReplyAliveTimeout(1000 * 60);
+        socketClient.setCharsetName("UTF-8");
+        socketClient.connect();
+    }
+}

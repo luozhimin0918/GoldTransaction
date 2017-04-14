6b147ef277c428cd667c36c01fa660c8c722ae23
Android-罗志敏 <54543534@domain.com>
2017-04-14 16:32:13 +0800

    可以发送数据了11111111

--- app/src/main/java/com/kxt/goldtransaction/MainActivity.java
+++ app/src/main/java/com/kxt/goldtransaction/MainActivity.java
@@ -4,19 +4,26 @@ import android.net.wifi.WifiInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
 import com.google.gson.Gson;
 import com.kxt.goldtransaction.bean.LoginBean;
+import com.kxt.goldtransaction.util.Base64Utils;
 import com.kxt.goldtransaction.util.NetUtils;
 import com.kxt.goldtransaction.util.RSAUtils;
 import com.socks.library.KLog;
 import com.vilyever.socketclient.SocketClient;
 import com.vilyever.socketclient.SocketResponsePacket;
 
+import org.apache.commons.io.output.ByteArrayOutputStream;
+
+import java.security.KeyPair;
 import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,28 +31,11 @@ public class MainActivity extends AppCompatActivity {
     private SocketClient socketClient;
     private String url="117.141.138.101";
     private  int prot=41901;
-    private static String PUCLIC_KEY_TOW =
+    private static String PUCLIC_KEY_TOW ="MIICRzCCAbACCQCEwTrLj/zDfjANBgkqhkiG9w0BAQUFADBoMQswCQYDVQQGEwJDTjELMAkGA1UEChMCSVQxEzARBgNVBAMTCkdlc3NTZXJ2ZXIxEDAOBgNVBAgTB0d1YW5nWGkxEDAOBgNVBAcTB05hbk5pbmcxEzARBgNVBAsTCkdlc3NTZXJ2ZXIwHhcNMTYwOTI3MTA0NjA4WhcNMjYwOTI1MTA0NjA4WjBoMQswCQYDVQQGEwJDTjELMAkGA1UEChMCSVQxEzARBgNVBAMTCkdlc3NTZXJ2ZXIxEDAOBgNVBAgTB0d1YW5nWGkxEDAOBgNVBAcTB05hbk5pbmcxEzARBgNVBAsTCkdlc3NTZXJ2ZXIwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBANzrbkoPp8hIpqlOfuAWVms1f2NxfMnzcyTAo15pGKwlQerYWu8SpWviRDU57K97jfAyxOGApDCjKbMJJh2vPLaSb9VU/OUmwfqpyIKkoW2u3BG3Lr0IBJtosZOw1vfj+zltOZnizRyvnVDj3w+WOIB8jn88glqfmppNYRviavKRAgMBAAEwDQYJKoZIhvcNAQEFBQADgYEAOunehQrYZa9o+sh5qwg2x9rzgZ7EIu8JjiY3IEj2qu4RfuPu2PXbnwA6E+9GrKD3roQX33GmInXVmMlZg/pziYbuHQ3FfRSfjuegSwmWHgnt34LfX63KZoS/4XDTIUOfRoqsXDep1crVptksfvBwdruZ+uEckT8vf7UEAjC2+O8=";
 
-                            "MIICRzCCAbACCQCEwTrLj/zDfjANBgkqhkiG9w0BAQUFADBoMQswCQYDVQQGEwJD" +"\r"+
-                            "TjELMAkGA1UEChMCSVQxEzARBgNVBAMTCkdlc3NTZXJ2ZXIxEDAOBgNVBAgTB0d1" +"\r"+
-                            "YW5nWGkxEDAOBgNVBAcTB05hbk5pbmcxEzARBgNVBAsTCkdlc3NTZXJ2ZXIwHhcN" +"\r"+
-                            "MTYwOTI3MTA0NjA4WhcNMjYwOTI1MTA0NjA4WjBoMQswCQYDVQQGEwJDTjELMAkG" +"\r"+
-                            "A1UEChMCSVQxEzARBgNVBAMTCkdlc3NTZXJ2ZXIxEDAOBgNVBAgTB0d1YW5nWGkx" +"\r"+
-                            "EDAOBgNVBAcTB05hbk5pbmcxEzARBgNVBAsTCkdlc3NTZXJ2ZXIwgZ8wDQYJKoZI" +"\r"+
-                            "hvcNAQEBBQADgY0AMIGJAoGBANzrbkoPp8hIpqlOfuAWVms1f2NxfMnzcyTAo15p" +"\r"+
-                            "GKwlQerYWu8SpWviRDU57K97jfAyxOGApDCjKbMJJh2vPLaSb9VU/OUmwfqpyIKk" +"\r"+
-                            "oW2u3BG3Lr0IBJtosZOw1vfj+zltOZnizRyvnVDj3w+WOIB8jn88glqfmppNYRvi" +"\r"+
-                            "avKRAgMBAAEwDQYJKoZIhvcNAQEFBQADgYEAOunehQrYZa9o+sh5qwg2x9rzgZ7E" +"\r"+
-                            "Iu8JjiY3IEj2qu4RfuPu2PXbnwA6E+9GrKD3roQX33GmInXVmMlZg/pziYbuHQ3F" +"\r"+
-                            "fRSfjuegSwmWHgnt34LfX63KZoS/4XDTIUOfRoqsXDep1crVptksfvBwdruZ+uEc" +"\r"+
-                            "kT8vf7UEAjC2+O8=";
-    private static String PUCLIC_KEY="MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCfRTdcPIH10gT9f31rQuIInLwe"
-            + "7fl2dtEJ93gTmjE9c2H+kLVENWgECiJVQ5sonQNfwToMKdO0b3Olf4pgBKeLThra"
-            + "z/L3nYJYlbqjHC3jTjUnZc0luumpXGsox62+PuSGBlfb8zJO6hix4GV/vhyQVCpG"
-            + "9aYqgE7zyTRZYX9byQIDAQAB" ;
+    private static String PUCLIC_KEY="MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzdaTCixSKYD80y5KihoqHSjrq5a+qqIXsGJmP34RvQ5xC5GhX8dNyzfAgdYyOWhK/Jz699Xw+zNQXhpPVyTKns6dJlRadUiy1YBQ4/dC1zL5imw3QDRqgx+yT/vD7aBGXUww8wiwwOhGVFrXhVvJHjTNaLyoPOekHK1MNUeSK+HOeTVfY9MPcC/6kEDCxTGnjKgZhYmEATYEL6CKBA0+/wAS6iKJ2xnhoSYhfBEqGv2P/7lGrfhRDzG6JjXYR8s/YlGrzsbkqt8vVTNaGpRKo91H6Lfno3a4P0SfH2WX5jlhXhp7hAj9kNP+ryXA2KTb2JKiyN318p3wAgUK28CCZwIDAQAB";
 
-    String aaaaa="";
-    String bbbbb="1";
+   String abcdeStr="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,21 +46,72 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initData() {
+        String AAAA="";
+        String BBBB="1";
+        String CCCC="C080";
+        String DDDD="          ";
+        String BCD="";
+        int  BCDLength=0;
+        int  MIWENLength=0;
+        String encryStr="";//密文String
         LoginBean  loginBean =new LoginBean();
         loginBean.setOper_flag(1);
         loginBean.setUser_id("1089117276");
         loginBean.setPassword("5ea9144fa6afff4d0559d2f4a6c10eda");
         //判断wifi是否开启
         String ip="";
-          ip= NetUtils.getLocalIPAddress();
+          ip= "192.168.2.179";
         loginBean.setLan_ip(ip);
-        KLog.json(JSON.toJSONString(loginBean));
+        String loginBeanStr =JSON.toJSONString(loginBean);
+        KLog.json(loginBeanStr);
         // 从字符串中得到公钥
         try {
-            PublicKey publicKey = RSAUtils.loadPublicKey(PUCLIC_KEY);
+            // 公钥
+//            KeyPair keyPair=RSAUtils.generateRSAKeyPair(RSAUtils.DEFAULT_KEY_SIZE);
+//            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();//自动生成key
+//            PublicKey publicKey=RSAUtils.loadPublicKey(PUCLIC_KEY);//字符串取生成key
+//           PublicKey publicKey =RSAUtils.loadPublicKey(PUCLIC_KEY);//字符串取生成key2
+
+
+            // 从文件中得到公钥
+            InputStream jjjj=this.getClass().getClassLoader().getResourceAsStream("assets/"+"rsa_public_key.pem");
+            PublicKey publicKey = RSAUtils.loadPublicKey(jjjj);
+
+           RSAUtils.printPublicKeyInfo(publicKey);
+//            byte[] encryptBytes=    RSAUtils.encryptByPublicKey(loginBeanStr.getBytes(),publicKey.getEncoded());//直接加密
+            byte[] encryptBytes=RSAUtils.encryptData(loginBeanStr.getBytes(),publicKey);//分成100字节一个个加密
+            encryStr= Base64Utils.encode(encryptBytes);
+            MIWENLength=encryptBytes.length;
+            KLog.d("密文的字节长度："+MIWENLength);
+            KLog.d(encryStr);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+        BCD=BBBB+CCCC+DDDD;
+        byte[] sd =Base64Utils.decode(BCD);
+        BCDLength=sd.length;
+        KLog.d("BCD的字节长度："+BCD+">>>>"+BCDLength);
+
+        int ZJLeng =BCDLength+MIWENLength;
+
+
+       int ziLeng=String.valueOf(ZJLeng).length();
+        KLog.d("总的字节长度："+ZJLeng+"  "+ziLeng);
+        String  ziStrig="";
+        if(ziLeng<8){
+
+            int l =8-ziLeng;
+            for(int f=0;f<l;f++){
+                ziStrig+="0";
+            }
+            ziStrig+=ZJLeng;
+            KLog.d("表示的8位数："+ziStrig);
+        }
+        abcdeStr=ziStrig+BCD+encryStr;
+
+        KLog.d("abcde："+abcdeStr);
+
     }
 
 
@@ -80,6 +121,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onConnected(SocketClient client) {
                 Log.d("socketTO","onConnected.......");
+                if(!TextUtils.isEmpty(abcdeStr)){
+//                    socketClient.send(Base64Utils.decode(abcdeStr));
+                    socketClient.sendBytes(Base64Utils.decode(abcdeStr));
+                }
 //                socketClient.send("hello, server !--------------------------->Android");
 //                socketClient.setHeartBeatMessage("hello, server !--------------------------->Android");
             }
--- app/src/main/java/com/kxt/goldtransaction/util/RSAUtils.java
+++ app/src/main/java/com/kxt/goldtransaction/util/RSAUtils.java
@@ -1,33 +1,109 @@
 package com.kxt.goldtransaction.util;
 
+
+import java.io.ByteArrayOutputStream;
+import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+import java.io.InputStream;
+import java.io.IOException;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import javax.crypto.Cipher;
+
+import static com.kxt.goldtransaction.util.Constart.RSA;
+import static com.kxt.goldtransaction.util.Constart.RSAPAD;
 
 /**
  * Created by Administrator on 2017/4/13.
  */
 
 public class RSAUtils {
+    public static final int DEFAULT_KEY_SIZE = 2048;//秘钥默认长度
+
     /**
-     * 从字符串中加载公钥
+     * 打印公钥信息
      *
-     * @param publicKeyStr
-     *            公钥数据字符串
-     * @throws Exception
-     *             加载公钥时产生的异常
+     * @param publicKey
      */
+    public static void printPublicKeyInfo(PublicKey publicKey)
+    {
+        RSAPublicKey rsaPublicKey = (RSAPublicKey) publicKey;
+        System.out.println("----------RSAPublicKey----------");
+        System.out.println("PublicKeyString: "+Base64Utils.encode(rsaPublicKey.getEncoded()));
+        System.out.println("Modulus.length=" + rsaPublicKey.getModulus().bitLength());
+        System.out.println("Modulus=" + rsaPublicKey.getModulus().toString());
+        System.out.println("PublicExponent.length=" + rsaPublicKey.getPublicExponent().bitLength());
+        System.out.println("PublicExponent=" + rsaPublicKey.getPublicExponent().toString());
+    }
+
+    public static void printPrivateKeyInfo(PrivateKey privateKey)
+    {
+        RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) privateKey;
+        System.out.println("----------RSAPrivateKey ----------");
+        System.out.println("Modulus.length=" + rsaPrivateKey.getModulus().bitLength());
+        System.out.println("Modulus=" + rsaPrivateKey.getModulus().toString());
+        System.out.println("PrivateExponent.length=" + rsaPrivateKey.getPrivateExponent().bitLength());
+        System.out.println("PrivatecExponent=" + rsaPrivateKey.getPrivateExponent().toString());
+
+    }
+    /**
+     * 随机生成RSA密钥对
+     *
+     * @param keyLength 密钥长度，范围：512～2048
+     *                  一般1024
+     * @return
+     */
+    public static KeyPair generateRSAKeyPair(int keyLength) {
+        try {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance(RSA);
+            kpg.initialize(keyLength);
+            return kpg.genKeyPair();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 用公钥对字符串进行加密
+     *
+     * @param data 原文
+     */
+    public static byte[] encryptByPublicKey(byte[] data, byte[] publicKey) throws Exception {
+        // 得到公钥
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKey);
+        KeyFactory kf = KeyFactory.getInstance(RSA);
+        PublicKey keyPublic = kf.generatePublic(keySpec);
+        // 加密数据
+        Cipher cp = Cipher.getInstance(Constart.RSAPAD);
+        cp.init(Cipher.ENCRYPT_MODE, keyPublic);
+        return cp.doFinal(data);
+    }
+
+        /**
+         * 从字符串中加载公钥
+         *
+         * @param publicKeyStr
+         *            公钥数据字符串
+         * @throws Exception
+         *             加载公钥时产生的异常
+         */
     public static PublicKey loadPublicKey(String publicKeyStr) throws Exception
     {
         try
         {
             byte[] buffer = Base64Utils.decode(publicKeyStr);
-            KeyFactory keyFactory = KeyFactory.getInstance(Constart.RSA);
+            KeyFactory keyFactory = KeyFactory.getInstance(RSA);
             X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
             return keyFactory.generatePublic(keySpec);
         } catch (NoSuchAlgorithmException e)
@@ -41,7 +117,53 @@ public class RSAUtils {
             throw new Exception("公钥数据为空");
         }
     }
+    /**
+     * 从文件中输入流中加载公钥
+     *
+     * @param in
+     *            公钥输入流
+     * @throws Exception
+     *             加载公钥时产生的异常
+     */
+    public static PublicKey loadPublicKey(InputStream in) throws Exception
+    {
+        try
+        {
+            return loadPublicKey(readKey(in));
+        } catch (IOException e)
+        {
+            throw new Exception("公钥数据流读取错误");
+        } catch (NullPointerException e)
+        {
+            throw new Exception("公钥输入流为空");
+        }
+    }
+    /**
+     * 读取密钥信息
+     *
+     * @param in
+     * @return
+     * @throws IOException
+     */
+    private static String readKey(InputStream in) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String readLine = null;
+        StringBuilder sb = new StringBuilder();
+        while ((readLine = br.readLine()) != null)
+        {
+            if (readLine.charAt(0) == '-')
+            {
+                continue;
+            } else
+            {
+                sb.append(readLine);
+                sb.append('\r');
+            }
+        }
 
+        return sb.toString();
+    }
 
     /**
      * 用公钥加密 <br>
@@ -52,14 +174,14 @@ public class RSAUtils {
      *            公钥
      * @return 加密后的byte型数据
      */
-    public static byte[] encryptData(byte[] data, PublicKey publicKey)
+    public static byte[] encryptData(byte[] data, PublicKey publicKey) throws IOException,GeneralSecurityException
     {
         try
         {
-            Cipher cipher = Cipher.getInstance(Constart.RSA);
+            Cipher cipher = Cipher.getInstance(RSAPAD);
             // 编码前设定编码方式及密钥
             cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-            ByteBufferOutputStream  byteBufferOutputStream=new ByteBufferOutputStream();
+            ByteArrayOutputStream  byteArrayOutputStream =new ByteArrayOutputStream();
             for(int i=0;i<data.length;i=i+100){
                 int len=0;
                 if((i+100)>data.length){
@@ -69,10 +191,10 @@ public class RSAUtils {
                 }
                 byte[] tmp=new byte[len];
                 System.arraycopy(data,i,tmp,0,tmp.length);
-                byteBufferOutputStream.write(cipher.doFinal(tmp));
+                byteArrayOutputStream.write(cipher.doFinal(tmp));
             }
             // 传入编码数据并返回编码结果
-            return byteBufferOutputStream.toString().getBytes();
+            return byteArrayOutputStream.toByteArray();
         } catch (Exception e)
         {
             e.printStackTrace();
